95443757860a7550fa4035ab2a6b1f7452086f14
singularity014 <49594316+singularity014@users.noreply.github.com>
2020-06-01 03:17:59 +0200

    Update Encoder_Decoder_NMT.py

--- Encoder_Decoder_NMT.py
+++ Encoder_Decoder_NMT.py
@@ -11,6 +11,7 @@ en_len = 15
 en_vocab = 150
 hsize = 48
 
+# No matter how complex the model, the unederline concdept of encoder remains the same
 #  ---------------- ENCODER -----------------------------
 # Define an input layer
 en_inputs = keras.layers.Input(shape=(en_len, en_vocab))
