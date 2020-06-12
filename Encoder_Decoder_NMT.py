9152653dcdedc2e2e7cea3765b1cf06abc8f4469
singularity014 <49594316+singularity014@users.noreply.github.com>
2020-06-12 08:38:08 +0200

    Update Encoder_Decoder_NMT.py
    compiling model

--- Encoder_Decoder_NMT.py
+++ Encoder_Decoder_NMT.py
@@ -58,4 +58,13 @@ de_dense_time = TimeDistributed(de_dense)
 de_pred = de_dense_time(de_out)
 print("Prediction shape: ", de_pred.shape)
 
+# ------------------ Compiling Model ----------------------------
 
+# Define a model with encoder input and decoder output
+nmt = Model(inputs=en_inputs, outputs=de_pred)
+
+# Compile the model with an optimizer and a loss
+nmt.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
+
+# View the summary of the model 
+nmt.summary()
